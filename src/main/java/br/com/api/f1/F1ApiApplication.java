0ee98a786e22095ffc2d6b387243e54eb133963c
Ronaldo Feitosa <ronaldo.feitosa@mercadolivre.com>
2023-01-19 21:46:33 -0300

    update spring version

--- src/main/java/br/com/api/f1/F1ApiApplication.java
+++ src/main/java/br/com/api/f1/F1ApiApplication.java
@@ -1,11 +1,14 @@
 package br.com.api.f1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @EnableFeignClients
 @SpringBootApplication
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class F1ApiApplication {
 
 	public static void main(String[] args) {
